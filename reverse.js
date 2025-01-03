85960b402923ef817f9534bc30a2fb62057fddc0
Bainomugisha Libra <148781031+BainoLibra@users.noreply.github.com>
2025-01-03 05:13:06 +0300

    i have added vs code

---
+++ reverse.js
@@ -0,0 +1,12 @@
+let firstName =" Libra ";
+// console.log(firstName.reverse());
+console.log(firstName.slice(0, 4));
+console.log(firstName.substring(3, 10));
+console.log(firstName.toUpperCase());
+console.log(firstName.toLowerCase());
+console.log(firstName.trim());
+console.log(firstName.replace("Libra", "Baino"));
+console.log(firstName.indexOf(""));
+console.log(firstName.split());
+console.log(firstName.split(""));
+console.log(firstName.includes("Li"))
