12b2aafec3b6662db9c5f98f2825892c99b48012
Bainomugisha Libra <148781031+BainoLibra@users.noreply.github.com>
2025-01-03 05:18:09 +0300

    Update reverse.js

--- reverse.js
+++ reverse.js
@@ -1,5 +1,5 @@
 let firstName =" Libra ";
-// console.log(firstName.reverse());
+console.log(firstName.reverse);
 console.log(firstName.slice(0, 4));
 console.log(firstName.substring(3, 10));
 console.log(firstName.toUpperCase());
@@ -10,3 +10,4 @@ console.log(firstName.indexOf(""));
 console.log(firstName.split());
 console.log(firstName.split(""));
 console.log(firstName.includes("Li"))
+console.log(typeof firstName)
